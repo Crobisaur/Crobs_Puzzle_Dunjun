b0015cc642e69a27db1e977f41255d21d8838884
Christo <crobiso2@gmail.com>
2015-12-20 19:33:58 -0500

    First Commit

---
+++ include/Puzzle_Dunjun/common.hpp
@@ -0,0 +1,15 @@
+#ifndef PUZZLE_DUNJUN_COMMON_HPP
+#define PUZZLE_DUNJUN_COMMON_HPP
+
+namespace Puzzle_Dunjun
+{
+using float32 = float;
+using float64 = double;
+}// namespace Puzzle_Dunjun
+
+#define GLOBAL static
+#define INTERNAL static
+#define LOCAL_PERSIST static
+
+
+#endif
